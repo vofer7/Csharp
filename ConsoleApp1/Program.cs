f35fba1e587f5ca5bc18a2a1ac26b5df9034d5be
Przemek <vofer7@gmail.com>
2019-04-06 19:08:06 +0200

    Pierwszy Program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+                Console.WriteLine("Witaj ," + item);
+            }
         }
     }
 }
