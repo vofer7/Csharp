fc3910f2d605bfbd82daba0eeec5719e522ffea4
Przemek <vofer7@gmail.com>
2019-04-16 17:32:49 +0200

    sdadas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,46 @@
 
 namespace ConsoleApp1
 {
-    class Program
+   class Program
     {
+      
         static void Main(string[] args)
         {
-            foreach (var item in args)
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Podaj swoje imie ");
+            string name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            greetings(name);
+            int age;
+            int licznik = 1;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+            Data(ref age, ref result);
+
+            Console.ReadLine();
+        }
+
+        private static void Data(ref int age, ref bool result)
+        {
+            if (result == false)
             {
-                Console.WriteLine("Witaj ," + item);
+                Console.WriteLine("Zle dane , wpisz jeszcze raz");
+                do
+                {
+
+                    result = int.TryParse(Console.ReadLine(), out age);
+
+                } while (age == 0);
+
+                Console.WriteLine("tera wiek to" + age);
             }
         }
+
+        private static void greetings(string name)
+        {
+            Console.WriteLine("Witaj  " + name);
+            Console.WriteLine("Podaj swoj wiek  ");
+        }
+
     }
 }
